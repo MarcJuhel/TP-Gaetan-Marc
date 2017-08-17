976f5466e079fbb7505201d222003b858ed9a8b7
Ashijo <Ashijo@users.noreply.github.com>
2017-08-17 15:32:41 -0400

    Add files via upload

--- TP/views/page_bottom.php
+++ TP/views/page_bottom.php
@@ -2,21 +2,31 @@
 /**
  *
  */
+
+$date = date(j, time()).date(F,time()).date(y ,time());
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title></title>
-</head>
-<body>
+
 <footer>
 
+
     <?php
-    $result = $_GET['../image'];
-    echo '<img src ="../image/amazon-footer/'.$result.'.png">';
+
+
+    require_once 'menu.php';
+
+    echo '<span id="tispan" class="footer span date">Site mise a jour le'.$date.'</span>';
+
+    echo '<span id="coor1" class="footer span">Nos coordonnées : </span> <span id="coor2" class="footer span">12345 rue de la chaudière <br>H2D 4P5<br>Montréal</span>';
+
+
+    echo '<div id="rezosocio">';
+    echo '<a class="rezo footer" href="www.facebook.com"><img class="rezo footer" src="../image/fb.jpg"></a>';
+    echo '<a class="rezo footer" href="www.twitter.com"><img class="rezo footer" src="../image/twit.png"></a>';
+    echo '<a class="rezo footer" href="www.instagram.com"><img class="rezo footer" src="../image/inst.jpg"></a>';
+    echo '<a class="rezo footer" href="www.youtube.com"><img class="rezo footer" src="../image/yt.jpg"></a>';
+    echo '</div>';
+
     ?>
 
 </footer>
-</body>
-</html>
