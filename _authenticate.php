88081724b07ec8efa25159d8e6eef1b53ccc5e56
MarcJuhel <juhelmarc@gmail.com>
2017-08-17 10:35:36 -0400

    Add files via upload

---
+++ _authenticate.php
@@ -0,0 +1,10 @@
+<?php
+
+function authenticate($username, $password) {
+    $users = array(
+        'brice' => '456',
+        'hatem' => '789',
+        'tania' => '101',
+    );
+    return true;
+}
