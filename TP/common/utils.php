72242768ce0c6e5aec7ece97c30c6623d3fd3e01
Ashijo <ligno19@gmail.com>
2017-08-18 11:32:26 -0400

    fonction creatheader

--- TP/common/utils.php
+++ TP/common/utils.php
@@ -7,6 +7,21 @@
  */
 
 
+function creatHeader($titre, $description, $cssPath, $jsPath){
+echo '<!DOCTYPE html><html lang="fr"><head><meta charset="utf-8">
+    	<title>'.$titre.'</title><meta name="author" content="Marc Camil Gaetan" >
+        <meta name="description" content="'.$description.'
+        " ><link rel="stylesheet" href="'.$cssPath.'"> 
+    	<script src="'.$jsPath.'"></script>
+		
+		<!--[if lt IE 9]>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<![endif]-->
+	</head>';
+
+};
+
+
 /**
  * Encrypter un mot de passe
  * @param $password : Le mot de passe à encrypter
