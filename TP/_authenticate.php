03d7bdc81d146a56d715c8e6ac633f543e5a6607
Ashijo <ligno19@gmail.com>
2017-08-18 11:44:51 -0400

    des choses

--- TP/_authenticate.php
+++ TP/_authenticate.php
@@ -1,11 +1,21 @@
 <?php
+require_once 'views/page_top.php';
+$image = "http://138.68.233.236/wp-content/uploads/2015/07/amazon-2015-v2.png";
+require_once 'common/utils.php';
+creatHeader('Nozama','Site de vente de goodies et bandes dessiné nippon', 'style/main.css','js/main.js');
+echo '<html><body>';
+require_once 'views/page_top.php';
 
-function authenticate($username, $password) {
-    $users = array(
-        'brice' => '456',
-        'hatem' => '789',
-        'tania' => '101',
-    );
-    return array_key_exists($username,$users)&&(md5($password) === $users[$username]);
-//var_dump($users);
-}
+
+
+
+
+
+
+
+
+
+
+
+require_once 'views/page_bottom.php';
+echo '</body></html>';
--- TP/catalogue.php
+++ TP/catalogue.php
@@ -1,18 +1,21 @@
-
 <?php
-/**
- *
- */
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title></title>
-</head>
-<body>
-<?php 
-?>
+require_once 'views/page_top.php';
+$image = "http://138.68.233.236/wp-content/uploads/2015/07/amazon-2015-v2.png";
+require_once 'common/utils.php';
+creatHeader('Nozama','Site de vente de goodies et bandes dessiné nippon', 'style/main.css','js/main.js');
+echo '<html><body>';
+require_once 'views/page_top.php';
 
-</body>
-</html>
+
+
+
+
+
+
+
+
+
+
+
+require_once 'views/page_bottom.php';
+echo '</body></html>';
