37d5745144b06a0bfb3cb855a6ec2f24723fc18b
Ashijo <ligno19@gmail.com>
2017-08-20 21:17:10 -0400

    descriptions

--- TP/panier.php
+++ TP/panier.php
@@ -3,7 +3,7 @@ session_start();
 if(!array_key_exists('login',$_SESSION)){$_SESSION['login']=false;};
 $image = "http://138.68.233.236/wp-content/uploads/2015/07/amazon-2015-v2.png";
 require_once 'common/utils.php';
-creatHeader('Nozama','Site de vente de goodies et bandes dessiné nippon', 'style/main.css','js/main.js');
+creatHeader('Nozama votre panier','', 'style/main.css','js/main.js');
 echo '<html><body>';
 require_once 'views/page_top.php';
 
